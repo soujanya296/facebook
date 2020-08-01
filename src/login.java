c8361e0a29df5085f45cc10cef3ae6dc42c5597d
Soujanya C K <Soujanya C K@LAPTOP-R4RV00HA>
2020-08-01 16:18:08 +0530

    login script

--- src/login.java
+++ src/login.java
@@ -1,9 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class login {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.gecko.driver", "./Softwares/geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.facebook.com/");	
 	}
 
 }
